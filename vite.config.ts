499044c7ac26d0e5b51a61434bbe6376888031a9
JosiMendoza <jos.mendozal@duocuc.cl>
2025-06-05 20:27:25 -0400

    GG3

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://JosiMendoza.github.io/APKminera/',
+  base:'https://JoseML23.github.io/APKminera/',
 })
